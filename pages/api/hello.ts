10578cfa0952e99ddbbebca7743f2803e149df4c
jiayishen21 <lucasshen21@gmail.com>
2023-12-26 11:08:45 -0500

    example api call

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -2,12 +2,14 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+  name?: string,
+  message?: string | undefined,
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  // res.status(200).json({ name: 'John Doe' })
+  res.status(400).json({ message: 'failed' })
 }
