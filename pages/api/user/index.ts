732246913d310af4988f027c08719f1069e9aa9a
jiayishen21 <lucasshen21@gmail.com>
2023-12-29 17:55:09 -0500

    fetch user api

--- pages/api/user/index.ts
+++ pages/api/user/index.ts
@@ -3,6 +3,7 @@ import { UserType } from '../../../types'
 import connectDB from '../../../utils/connectDB'
 import User from '../../../models/userModel'
 import decrypt from '../../../utils/decrypt'
+import authenticate from '../../../utils/authenticate'
 
 type Data = {
   user?: UserType,
@@ -20,12 +21,19 @@ export default async function handler(
 
     await connectDB()
 
-    const { encryptedJWT, iv, clientKey } = req.body
+    const { iv, clientKey } = req.query
 
-    const jwt = decrypt(encryptedJWT, iv, clientKey, process.env.ECC_PRIVATE_KEY || '')
+    if (typeof iv !== 'string' || typeof clientKey !== 'string') {
+      throw new Error('Encryption error.')
+    }
 
+    const jwt = decrypt(req.headers.authorization || '', iv, clientKey, process.env.ECC_PRIVATE_KEY || '')
 
-    res.status(200).json({ message: 'good' })
+    await connectDB()
+
+    const user = await authenticate(jwt)
+
+    res.status(200).json({ user })
   } catch (error: any) {
     res.status(400).json({ message: error.message })
   }
