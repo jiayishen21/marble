2bbbc69af9dcb4d1b0299b1a6d685a8829e0ec90
Andrew Durnford <andrewhdurnford@gmail.com>
2023-12-30 14:23:13 -0500

    started landing page

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -5,15 +5,15 @@ import Link from "next/link";
 export default function navBar() {
   return (
     <>
-      <nav className="flex items-center justify-between w-screen p-[1rem]">
+      <nav className="flex items-center justify-between w-screen h-1/4 p-[1rem]">
         <Link href="/">
-          <div className="">
-            <Image src="./marble.svg" alt="marble svg" width={50} height={50} />
+          <div className="w-96 h-32">
+            <Image className="w-auto max-h-full" src="./marble.svg" alt="marble svg" width={1000} height={50} />
           </div>
         </Link>
 
-        <div>
-          <ul className="flex justify-center items-center">
+        <div className="flex-initial w-5/12 mr-14 h-full">
+          <ul className="flex justify-between items-center mb-12 text-3xl">
             <Link href="/">
               <li>About us</li>
             </Link>
