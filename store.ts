0b0bac58c4d3a9d3e3c03ff1722bb25a2aa2d17c
jiayishen21 <lucasshen21@gmail.com>
2024-01-06 18:00:25 -0500

    delete yarn lock file for vercel deployment

--- store.ts
+++
@@ -1,9 +0,0 @@
-// store/store.js
-import { configureStore } from '@reduxjs/toolkit';
-
-export const store = configureStore({
-  reducer: {
-    // your reducers here
-  },
-  // middleware, devTools, and other configurations can be added here
-});
