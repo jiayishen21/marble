c071b11addcc106a47c9c276a7b0a2243d1fd6ee
jiayishen21 <lucasshen21@gmail.com>
2024-01-12 00:17:34 -0500

    rename

--- pages/dashboard/transaction.tsx
+++ pages/dashboard/transaction.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import DashboardTabs from "../../components/DashboardTabs";
+import DashboardTabs from "../../components/DashboardTabss";
 
 export default function transaction() {
   return (
