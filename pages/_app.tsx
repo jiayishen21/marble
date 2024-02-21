d46432f2741e55adac8ee06b0358b687324d2ed3
jiayishen21 <lucasshen21@gmail.com>
2024-02-21 18:03:14 -0500

    readd fetching poll and share data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -64,7 +64,6 @@ const AppComponent: React.FC<AppProps> = ({ Component, pageProps }) => {
       easing: "ease-out",
     });
 
-    let userFound = false
     axios
       .get("/api/user")
       .then((response: any) => {
@@ -72,15 +71,11 @@ const AppComponent: React.FC<AppProps> = ({ Component, pageProps }) => {
           dispatch(setUser(response.data.user));
         }
         dispatch(setUserLoading(false));
-        userFound = true
       })
       .catch(() => {
         dispatch(setUserLoading(false));
       });
 
-    if (!userFound) {
-      return
-    }
     axios
       .get("/api/shares")
       .then((response: any) => {
